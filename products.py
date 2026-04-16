8faa0a69ff9be3fa9215014ba7a02b130eb7564d
Romy Barnt <112576159+kbrh3@users.noreply.github.com>
2026-04-16 17:07:23 -0500

    Update products.py

--- products.py
+++ products.py
@@ -1,1 +1,17 @@
-
+products = [
+    {
+        "title": "Red Running Shoes",
+        "category": "shoes",
+        "attributes": "sport, lightweight"
+    },
+    {
+        "title": "Luxury Perfume Bottle",
+        "category": "cosmetics",
+        "attributes": "glass, elegant, minimal"
+    },
+    {
+        "title": "Black Witch Hat",
+        "category": "costume",
+        "attributes": "pointed, fabric, Halloween"
+    }
+]
